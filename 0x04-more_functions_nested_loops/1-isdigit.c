1347ec37a24c19d35424193c901a5d91833a7881
uh5aM <ahmadabubakarmusa0@gmail.com>
2023-09-14 12:35:12 -0700

    done

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,17 @@
-#include "main.h"
-#include <stdio.h>
+#include "holberton.h"
+
 /**
-*_isupper - function that verifies if a  character is uppercase or not
-*@c: tested character
-*Return: returns 1 if it is uppercase , 0 if not
-*/
-
-int _isupper(int c)
+ * mul -  checks for checks for a digit (0 through 9).
+ * @a: a -  Variable
+ * @b: b - variable
+ * Return: Always 0.
+ */
+int mul(int a, int b)
 {
-if ((c >= 'A') && (c <= 'Z'))
-return (1);
 
-return (0);
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
 }
