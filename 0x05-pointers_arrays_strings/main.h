20263445de0bd8d56791bf4af49320a47b11225d
uh5aM <ahmadabubakarmusa0@gmail.com>
2023-09-18 14:27:42 -0700

    just

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,19 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 
-void reset_to_98(int *);
-
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int n;
-
-	n = 402;
-	printf("%d\n", n);
-	reset_to_98(&n);
-	printf("%d\n", n);
-	return (0);
-}
+* void prototypes()
+*char prototypes()
+* int prototypes()
+*/
+void reset_to_98(int *n);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void swap_int(int *a, int *b);
+void swap_char(char *a, char *b);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _putchar(char c);
+int _strlen(char *s);
+int _atoi(char *s);
+#endif /*MAIN_H*/
