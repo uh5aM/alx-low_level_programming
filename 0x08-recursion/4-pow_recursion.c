fff985ff3e3efae65f250de6d2e90d7a1f2ade28
uh5aM <ahmadabubakarmusa0@gmail.com>
2023-09-29 11:44:22 -0700

    fixed

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -6,12 +6,14 @@ int _pow_recursion(int x, int y)
 {
 	if (y == 0)
 	{
-	return (1);
+		return (0);
+
 	}
 
 	if (y < 0)
 	{
-	return(-1);
+		return (-1);
+
 	}
 	return (x * _pow_recursion(x, y-1));
 
