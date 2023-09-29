a04855fc63cb0e62d0abaa7de411b1eefdd3ba65
uh5aM <ahmadabubakarmusa0@gmail.com>
2023-09-29 11:47:48 -0700

    now fixed

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 /*this is the how to rint the value of x and y 
  *
- * telling the system to print -i*/
+ * telling the system to print -i
+ */
 int _pow_recursion(int x, int y)
 {
 	if (y == 0)
 	{
 		return (1);
-
 	}
-
 	if (y < 0)
 	{
 		return (-1);
-
 	}
-	return (x * _pow_recursion(x, y-1));
-
+	return (x * _pow_recursion(x, y - 1));
 }
