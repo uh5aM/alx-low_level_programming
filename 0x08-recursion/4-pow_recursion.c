2d8e8d3ca25e90b0786952d22504d38a0ef43a2c
uh5aM <ahmadabubakarmusa0@gmail.com>
2023-09-29 11:41:39 -0700

    goodwork

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*this is the how to rint the value of x and y 
+ *
+ * telling the system to print -i*/
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+	return (1);
+	}
+
+	if (y < 0)
+	{
+	return(-1);
+	}
+	return (x * _pow_recursion(x, y-1));
+
+}
