30f1b9b0a4c7ba0b24d63d58af083d0979eaee97
uh5aM <ahmadabubakarmusa0@gmail.com>
2023-09-29 12:49:45 -0700

    done

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,13 @@
 #include "holberton.h"
-/*this is the how to rint the value of x and y 
+/**
+ * _pow_recursion - Write a function that returns the value of x raised to the
+ * power of y.
  *
- * telling the system to print -i
+ * @x: This is the input number
+ * @y: This is the power number
+ *
+ * Return: if "y" is bigger than zero raised to the power of "y",
+ * return -1 if "y" is lower than zero, return 1 if "y" is equal to 1, 
  */
 int _pow_recursion(int x, int y)
 {
