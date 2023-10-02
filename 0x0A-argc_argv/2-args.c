533894233f4e0a5ed127f122c138a2789e9291b2
uh5aM <ahmadabubakarmusa0@gmail.com>
2023-10-02 13:01:12 -0700

    im i right?

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*thi is te programe that print all the argument 
+ * it recives
+ * including the first one
+ * it prints only one argument pay line
+ */
+int main(int argc, char *argv[])
+{
+	int = 0;
+
+	for (i = 0, i < argc, i++);
+	{
+		printf("%s\n", argv[i]);
+	}
+	return 0;
+}
