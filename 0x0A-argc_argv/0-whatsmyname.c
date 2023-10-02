66e1251748d89549affab33955e94ca983a4c809
uh5aM <ahmadabubakarmusa0@gmail.com>
2023-10-02 12:41:41 -0700

    argv_argc

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    // Check if at least one argument (program name) is provided
-    if (argc >= 1) {
-        // Print the program name (including path) followed by a new line
-        printf("%s\n", argv[0]);
-    } else {
-        // Handle the case when no arguments are provided (unlikely in most scenarios)
-        printf("No program name provided.\n");
-    }
-    return 0;
+/**
+ * main - program that prints its name, followed by a new line
+ * @argc: number of arguments
+ * @argv: array with the arguments
+ *
+ * Return: always 0
+ **/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
 }
