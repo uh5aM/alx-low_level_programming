0b19013d79252f55588db96ae6b27224245a4bd2
uh5aM <ahmadabubakarmusa0@gmail.com>
2023-10-02 13:04:07 -0700

    this is right?

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
-/*thi is te programe that print all the argument 
- * it recives
- * including the first one
- * it prints only one argument pay line
- */
+
+/**
+ * main - program that prints all arguments it receives.
+ * @argc: number of arguments
+ * @argv: array with the arguments
+ *
+ * Return: always 0
+ **/
+
 int main(int argc, char *argv[])
 {
-	int = 0;
+	int i = 0;
 
-	for (i = 0, i < argc, i++);
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
-	return 0;
+	return (0);
 }
