16494d7dc978b025efcb5df3a2f4a37db66f63a4
uh5aM <ahmadabubakarmusa0@gmail.com>
2023-12-07 11:11:21 -0800

    huus

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (head->prev != NULL)
+		head = head->prev;
+
+	i = 0;
+
+	while (head != NULL)
+	{
+		if (i == index)
+			break;
+		head = head->next;
+		i++;
+	}
+
+	return (head);
+}
