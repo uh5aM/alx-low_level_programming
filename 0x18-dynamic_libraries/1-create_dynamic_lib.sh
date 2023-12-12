eb61a5687daa1a5a798c971dc64063dbc520e6d1
uh5aM <ahmadabubakarmusa0@gmail.com>
2023-12-11 23:58:57 -0800

    ?

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -Wall -Wextra - Warrior -pedantic -c -fPIC *.c
-gcc -shared -o libdynamic.so *.o
+gcc -wall -wextra -warror -pedantic -c -fPIC *.c
+gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
